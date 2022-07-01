3876f9644b0109ba515ae7105f4ab3f3ae8642cb
YangSH <2512445376@qq.com>
2022-07-01 20:31:44 +0800

    axios

---
+++ axios.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: "http://127.0.0.1",
+    timeout: 6000
+})
+
+instance.interceptors.request.use((config) => {
+    return config
+}, (error) => {
+    return Promise.reject(error)
+})
+
+instance.interceptors.response.use((response) => {
+    return response.data
+}, (error) => {
+    return Promise.reject(error)
+})
+
+
+export async function doPost(url, data, config) {
+    try {
+        return await instance.post(url, data, config)
+    } catch (e) {
+        console.log(url, e)
+    }
+}
+
+export async function doDelete(url, data, config) {
+    try {
+        return await instance.delete(url, config)
+    } catch (e) {
+        console.log(url, e)
+    }
+}
+
+export async function doPut(url, data, config) {
+    try {
+        return await instance.put(url, data, config)
+    } catch (e) {
+        console.log(url, e)
+    }
+}
+
+export async function doGet(url, config) {
+    try {
+        return await instance.get(url, config)
+    } catch (e) {
+        console.log(url, e)
+    }
+}
